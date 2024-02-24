5640471b910741872fb457b33f3128c0f8e5382c
Monica Garcia <monicairenegarcia@gmail.com>
2024-02-24 12:43:35 -0700

    complete testing user interfaces lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-  return <div>Your code here</div>;
+  return( 
+  <div>
+    <h1>Hi, I'm Monica</h1>
+    <img src="http://imgur.com/a/FxCW8It" alt='Monica Garcia photo'/>
+    <h2>About Me</h2>
+    <p>My name is Monica Garcia and I am a current student of FlatIron School's Software Engineering bootcamp. I am passionate about learning the ins and outs of computer programming and software engineering, and I look forward to continuously growing and expanding my skillset.</p>
+    <div>
+      <a href="https://github.com/belc0011">Monica Garcia's Git Hub Page</a>
+      </div>
+    <a href="www.linkedin.com/in/monica-garcia-81a7732b6">Monica Garcia's LinkedIn Profile</a>
+  </div>
+  )
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,4 +3,87 @@ import '@testing-library/jest-dom';
 
 import App from "../App";
 
-// Your tests here
+test("displays a top-level heading with the text `Hi, I'm _____`", () => {
+    render(<App />);
+
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    })
+
+    expect(topLevelHeading).toBeInTheDocument();
+})
+
+test("displays image of headshot", () => {
+    render(<App />);
+
+    const headShot = screen.getByRole("img", {
+        src: 'https://imgur.com/a/FxCW8It',
+        alt: /monica garcia photo/i,
+    })
+
+    expect(headShot).toBeInTheDocument();
+})
+
+test("image has source attribute", () => {
+    render(<App />);
+
+    const headShot2 = screen.getByRole("img", {
+        src: 'https://imgur.com/a/FxCW8It',
+        alt: /monica garcia photo/i,
+    })
+
+    expect(headShot2).toHaveAttribute('src')
+})
+
+test("displays 2nd level heading with text 'about me'", () => {
+    render(<App />);
+
+    const aboutMe = screen.getByRole("heading", {
+        name: /about me/i,
+        exact: false,
+        level: 2,
+    })
+
+    expect(aboutMe).toBeInTheDocument();
+})
+
+test("displays paragraph with biography", () => {
+    render(<App />);
+
+    const biography = screen.getByText(/my name is monica garcia/i);
+
+    expect(biography).toBeInTheDocument();
+})
+
+test("Has link to git hub page", () => {
+    render(<App />);
+
+    const gitHub = screen.getByText(/git hub/i);
+
+    expect(gitHub).toBeInTheDocument();
+})
+
+test("Git Hub Link has appropriate href attribute", () => {
+    render(<App />);
+
+    const gitHubLink = screen.getByText(/git hub/i);
+
+    expect(gitHubLink).toHaveAttribute('href', 'https://github.com/belc0011')
+})
+
+test("Has link to LinkedIn profile", () => {
+    render(<App />)
+
+    const linkedIn = screen.getByText(/linkedin/i);
+
+    expect(linkedIn).toBeInTheDocument();
+})
+test("LinkedIn Link has appropriate href attribute", () => {
+    render(<App />);
+
+    const linkedInLink = screen.getByText(/linkedin/i)
+
+    expect(linkedInLink).toHaveAttribute('href', 'www.linkedin.com/in/monica-garcia-81a7732b6')
+})
